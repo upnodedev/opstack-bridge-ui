98f3961fdcd512ef82ed25e203976de85eb65307
france <frongrider2@gmail.com>
2024-03-25 23:56:36 +0700

    fix route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,17 @@ import SelectChain from "@pages/SelectChain";
 import { createRef } from "react";
 
 const routes = [
-  { path: "/", element: <Bridge name="HomePage" />, nodeRef: createRef() },
   {
-    path: "/select-chain",
+    path: "/",
     element: <SelectChain name="HomePage" />,
     nodeRef: createRef(),
   },
+  {
+    path: "/bridge",
+    element: <Bridge name="HomePage" />,
+    nodeRef: createRef(),
+  },
+
   { path: "/toast", element: <Bridge />, nodeRef: createRef() },
 ];
 
